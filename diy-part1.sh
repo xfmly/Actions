834d185657ccccb61feea3c26f5b0b1287e50306
xfmly <58315920+xfmly@users.noreply.github.com>
2021-06-21 22:21:19 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,7 +14,7 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-sed -i '$a src-git src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
+sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
 
 # Add a feed source
 sed -i '$a src-git smartdns https://github.com/pymumu/smartdns' feeds.conf.default
